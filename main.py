57fcd6a88fd9408fd0bec7669c51ddc3e2dd5cd8
Giof272 <gsfarfan62@ucatolica.edu.co>
2025-11-18 15:46:56 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+deportistas = [
+    {"id": 1, "nombre": "Juan", "disciplina": "Fútbol"},
+    {"id": 2, "nombre": "Ana", "disciplina": "Natación"}
+]
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API FastAPI funcionando"}
+
+@app.get("/deportistas")
+def obtener_deportistas():
+    return deportistas
+
+@app.get("/buscar/{id}")
+def buscar(id: int):
+    for d in deportistas:
+        if d["id"] == id:
+            return d
+    return {"error": "No encontrado"}
